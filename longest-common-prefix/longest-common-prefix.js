629be2b31996a66f8eace2b0b0b23fbd4f932c37
Leira Sánchez <47611727+leira-sanchez@users.noreply.github.com>
2021-07-20 16:12:52 -0400

    Time: 60 ms (99.86%), Space: 38.8 MB (88.78%) - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs.length < 1) return "";
+    let prefix = strs[0];
+    
+    for (let i = 0; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+        }
+    }
+    return prefix;
+};
