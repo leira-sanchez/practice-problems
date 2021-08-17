f05f7ff1c9fcd870a1d00fd8e1dbd2d2ea2f69fb
Leira Sánchez <47611727+leira-sanchez@users.noreply.github.com>
2021-08-17 11:48:25 -0400

    Time: 212 ms (25.04%), Space: 50.6 MB (42.69%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.js
+++ find-the-duplicate-number/find-the-duplicate-number.js
@@ -2,18 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate = function(nums) {
-    if (nums.length === 1) return nums[0];
+// var findDuplicate = function(nums) {
+//     const counts = {};
     
-    let left = 0;
-    let right = 1;
-    const counts = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         if (counts[nums[i]]) {
+//             return nums[i];
+//         } else {
+//             counts[nums[i]] = 1;
+//         }
+//     }
+// };
+
+var findDuplicate = function(nums) {
+    nums.sort((a,b) => a - b);
     
     for (let i = 0; i < nums.length; i++) {
-        if (counts[nums[i]]) {
-            return nums[i];
-        } else {
-            counts[nums[i]] = 1;
-        }
+        if (nums[i] === nums[i+1]) return nums[i];
     }
 };
