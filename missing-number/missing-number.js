8b002c9b89babe400c3a6b13ff9d9979cc8904b3
Leira Sánchez <47611727+leira-sanchez@users.noreply.github.com>
2021-08-19 21:06:48 -0400

    Time: 96 ms (34.14%), Space: 41.4 MB (51.66%) - LeetHub

--- missing-number/missing-number.js
+++ missing-number/missing-number.js
@@ -2,15 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var missingNumber = function(nums) {
+// 88 ms
+//     const map = {};
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         map[nums[i]] = map[i] > 0 ? map[i]++ : 1;
+//     }
+    
+//     for (let i = 0; i < nums.length + 1; i++) {
+//         if (!map[i]) return i;
+//     }
+    
+// };
+
 var missingNumber = function(nums) {
-    const map = {};
+    nums.sort((a,b) => a - b);
     
     for (let i = 0; i < nums.length; i++) {
-        map[nums[i]] = map[i] > 0 ? map[i]++ : 1;
+        if (nums[i] !== i) return i;
     }
-    
-    for (let i = 0; i < nums.length + 1; i++) {
-        if (!map[i]) return i;
-    }
-    
+    return nums.length;
 };
