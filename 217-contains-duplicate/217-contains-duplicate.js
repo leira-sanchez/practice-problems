3f005db3f134ec38ec05f9e730df46f603e4e751
Leira Sánchez <47611727+leira-sanchez@users.noreply.github.com>
2022-06-22 13:48:28 -0400

    Time: 1870 ms (9.49%), Space: 49.9 MB (89.81%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -4,10 +4,17 @@
  */
 var containsDuplicate = function(nums) {
     // return nums.length !== new Set(nums).size;
-     let map = {};
+    //  let map = {};
+    // for (let i = 0; i < nums.length; i++) {
+    //   if (map[nums[i]]) return true;
+    //   map[nums[i]] = map[nums[i]] ? map[nums[i]] + 1 : 1;
+    // }
+    // return false;
+    
+    let arr = [];
     for (let i = 0; i < nums.length; i++) {
-      if (map[nums[i]]) return true;
-      map[nums[i]] = map[nums[i]] ? map[nums[i]] + 1 : 1;
+        if (arr.includes(nums[i])) return true;
+        arr.push(nums[i]);
     }
     return false;
 };
